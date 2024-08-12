65e0e4d95188ec4de00ab62f3f91e680e37d8379
Laden <delfanladen24@gmail.com>
2024-08-13 00:37:44 +0800

    adding video section

---
+++ app/components/Video.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Video = () => {
+  return (
+    <section className="relative md:p-20 flex gap-20 bg-gray-100">
+      <div className="p-20 absolute bg-red-500 left-0 top-10 w-[680px] h-[430px] rounded-r-[1.8rem]"></div>
+      <iframe
+        className="rounded-3xl shadow-xl w-full relative z-10 border-5 border-white "
+        width="640"
+        height="350"
+        src="https://www.youtube.com/embed/Xby6hbn1aAw?si=CRBcZbfoo1yYBcw8"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+      <div>
+        <h2 className="text-6xl font-bold flex flex-wrap">
+          Berada di Desa Jonggon
+        </h2>
+        <p className="text-medium text-gray-500 mt-5 leading-8">
+          Pada tahun 1970 masyarakat Suku Dayak Basab menjadi satu di Desa
+          Jonggon Desa antara Dayak Basab Jitan dengan masyarakat yang berada di
+          Desa Jonggon.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Video;
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import HeroImage from "./components/HeroImage";
 import Sejarah from "./components/Sejarah";
 import Lokasi from "./components/Lokasi";
 import Blog from "./components/Blog";
+import Video from "./components/Video";
 
 export default function Home() {
   return (
     <>
       <HeroImage />
+      <Video />
       <Sejarah />
       <Blog />
       <Lokasi />
