50e52c2b4cd60490ed57f62c215b60dbfe185c40
Laden <delfanladen24@gmail.com>
2024-08-13 16:00:15 +0800

    Create Responsive Video Section

--- app/components/HeroImage.tsx
+++ app/components/HeroImage.tsx
@@ -7,9 +7,9 @@ const HeroImage = () => {
       id="hero-image"
       className="p-4 pb-20 md:px-20 bg-gradient-to-b from-white from-70%  to-gray-100 relative mt-16 md:mt-24"
     >
-      <div className="hidden md:flex bg-red-500 p-10 rounded-full w-20 h-20 absolute top-14 -left-10"></div>
-      <div className="hidden md:flex bg-red-500 p-10 rounded-full w-20 h-20 absolute bottom-20 left-96"></div>
-      <div className="hidden md:flex bg-red-500 p-20 rounded-full w-44 h-44 absolute bottom-10 -right-20"></div>
+      <div className="hidden md:flex bg-red-400 p-10 rounded-full w-20 h-20 absolute top-14 -left-10"></div>
+      <div className="hidden md:flex bg-red-400 p-10 rounded-full w-20 h-20 absolute bottom-20 left-96"></div>
+      <div className="hidden md:flex bg-red-400 p-20 rounded-full w-44 h-44 absolute bottom-10 -right-20"></div>
       <div className=" mx-auto relative">
         <div className="grid grid-cols-1 md:grid-cols-2 sm:gap-4 md:gap-8 xl:gap-20 md:items-center">
           <div className="">
@@ -23,7 +23,7 @@ const HeroImage = () => {
             </p>
             <div className="my-7 grid gap-3 w-full sm:inline-flex">
               <Link
-                className="py-5 px-8 inline-flex text-lg justify-center items-center font-medium rounded-full text-white bg-red-600 hover:bg-red-700 focus:outline-none "
+                className="py-5 px-8 inline-flex text-lg justify-center items-center font-medium rounded-full text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none "
                 href="#sejarah"
               >
                 Mari Jelajahi
--- app/components/Video.tsx
+++ app/components/Video.tsx
@@ -1,9 +1,9 @@
 const Video = () => {
   return (
-    <section className="relative md:p-20 flex gap-20 bg-gray-100">
-      <div className="p-20 absolute bg-red-500 left-0 top-10 w-[680px] h-[430px] rounded-r-[1.8rem]"></div>
-      <iframe
-        className="rounded-3xl shadow-xl w-full relative z-10 border-5 border-white "
+    <section className="relative md:p-20 flex gap-16 bg-gray-100 p-4 flex-col md:flex-row justify-center">
+      <div className="p-20 shadow-lg absolute bg-red-500 left-0 -top-5 md:top-10 w-full lg:w-[680px] h-[430px] lg:rounded-r-[1.8rem]"></div>
+      <iframe 
+        className="rounded-3xl shadow-xl w-full relative z-10 border-2 border-white"
         width="640"
         height="350"
         src="https://www.youtube.com/embed/Xby6hbn1aAw?si=CRBcZbfoo1yYBcw8"
@@ -11,8 +11,8 @@ const Video = () => {
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
       ></iframe>
-      <div>
-        <h2 className="text-6xl font-bold flex flex-wrap">
+      <div className="relative z-10 md:bg-white  md:p-8 lg:p-20 rounded-3xl md:shadow-lg sm:top-10">
+        <h2 className="text-6xl font-bold flex flex-wrap ">
           Berada di Desa Jonggon
         </h2>
         <p className="text-medium text-gray-500 mt-5 leading-8">
