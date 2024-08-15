ea7ef560d5a0d07ccfa27b539507c15bcec19a3d
Laden <delfanladen24@gmail.com>
2024-08-15 17:45:15 +0800

    fixing code slug

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const Blog = (slug : string) => {
-  return (
-    <div>single blog</div>
-  )
-}
+const Page = ({ params }: { params: { slug: string } }) => {
+  return <div className="mt-28">{params.slug}</div>;
+};
 
-export default Blog
+export default Page;
