298d0521a498ea7b085376ba51da4f9352ddf3e2
Laden <delfanladen24@gmail.com>
2024-08-19 17:47:54 +0800

    improving blogs related

--- app/blog/[uid]/page.tsx
+++ app/blog/[uid]/page.tsx
@@ -1,4 +1,5 @@
 import Blog from "@/app/components/Blog";
+import CardComp from "@/app/components/card";
 import { RichText } from "@/app/components/RichText";
 import { createClient } from "@/prismicio";
 import { Button } from "@nextui-org/react";
@@ -12,6 +13,7 @@ export default async function Page({ params }: { params: Params }) {
   const page = await client
     .getByUID("blog", params.uid)
     .catch(() => notFound());
+  const blogs = await client.getAllByType("blog");
 
   return (
     <section className="pt-16 lg:pt-28 lg:pb-20 bg-gradient-to-b from-white to-gray-100 from-20%">
@@ -32,12 +34,30 @@ export default async function Page({ params }: { params: Params }) {
         <div className="flex justify-between py-8">
           <h4 className="text-4xl font-bold text-gray-900">Blog Lainnya</h4>
           <Link href="/blog" className="block">
-            <Button className="border-gray-700 text-gray-700 px-8 rounded-full" color="primary" variant="bordered">
+            <Button
+              className="border-gray-700 text-gray-700 px-8 rounded-full"
+              color="primary"
+              variant="bordered"
+            >
               Lihat Semua
             </Button>
           </Link>
         </div>
-        <Blog />
+        <div className="grid mb-8 gap-5 md:gap-10 sm:grid-cols-2 lg:grid-cols-3 sm:mx-auto lg:max-w-full">
+          {blogs.map(
+            (blog) =>
+              blog.uid !== page.uid && (
+                <CardComp
+                  key={blog.uid}
+                  title={blog.data.judul}
+                  date={blog.data.tanggal}
+                  image={blog.data.gambar}
+                  konten={blog.data.konten}
+                  uid={blog.uid}
+                />
+              )
+          )}
+        </div>
       </div>
     </section>
   );
